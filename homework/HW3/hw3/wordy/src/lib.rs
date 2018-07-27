c1e6bf16ca4b1f63ef6326632b8bbe8d53c10737
Jason Graalum <jasongraalum@gmail.com>
2018-07-26 17:55:54 -0700

    Final formatting

--- homework/HW3/hw3/wordy/src/lib.rs
+++ homework/HW3/hw3/wordy/src/lib.rs
@@ -1,4 +1,6 @@
-
+//Copyright (c) 2018 Jason Graalum
+//
+// Enum for WordProblem state machine
 enum Op {
     Start,
     Plus,
@@ -9,75 +11,85 @@ enum Op {
     Error,
 }
 
-
+// Simple struct holding WordProblem result
 pub struct WordProblem<'a> {
     result: Result<i32, &'a str>,
 }
 
+// WordProblem implementation
+//      new -> parse and return the result of the evaluation
+//      is_err() -> returns false if the result is an error
+//      answer() -> if result is of Ok() type, return the value
+//
 impl<'a> WordProblem<'a> {
-    pub fn new(problem : &'a str) -> Self {
+    pub fn new(problem: &'a str) -> Self {
         println!("{}", problem);
 
         //
-        // Ex: "What is -1 plus-10?"
+        // big harry function
+        // 1. Remove the ? from the end
+        // 2. Split by white space and filter "What", "is" and "by" <- remove by from multiplied by
+        // 3. fold the remaining elements through the state machine
         //
-        // Remove the ? from the end;
-        //
-        let result = problem.trim_right_matches('?')
-                .split_whitespace()
-                .filter(|s| s != &"by" && s != &"What" && s != &"is")
-                .fold((0, Op::Start), |(acc, curr_op), val| 
-                      match curr_op {
-                          Op::Error => (0, Op::Error),
-                          Op::Next => match val {
-                              "plus" => (acc, Op::Plus),
-                              "minus" => (acc, Op::Minus),
-                              "divided" => (acc, Op::Divided),
-                              "multiplied" => (acc, Op::Multiplied),
-                              _ => (0, Op::Error),
-                          },
-                          Op::Start => match val.parse::<i32>() {
-                                  Err(_) => (0, Op::Error),
-                                  Ok(v) => (v, Op::Next),
-                              },
-                          Op::Plus => match val.parse::<i32>() {
-                                           Err(_) => (0, Op::Error),
-                                           Ok(v) => (acc + v, Op::Next),
-                                       },
-                          Op::Minus => match val.parse::<i32>() {
-                                           Err(_) => (0, Op::Error),
-                                           Ok(v) => (acc - v, Op::Next),
-                                       },
-                          Op::Multiplied => match val.parse::<i32>() {
-                                           Err(_) => (0, Op::Error),
-                                           Ok(v) => (acc * v, Op::Next),
-                                       },
-                          Op::Divided => match val.parse::<i32>() {
-                                           Err(_) => (0, Op::Error),
-                                           Ok(v) => (acc / v, Op::Next),
-                                       },
-                      }
-                     ); 
+        let result = problem
+            .trim_right_matches('?')
+            .split_whitespace()
+            .filter(|s| s != &"by" && s != &"What" && s != &"is")
+            .fold((0, Op::Start), |(acc, curr_op), val| match curr_op {
+                Op::Error => (0, Op::Error),
+                Op::Next => match val {
+                    "plus" => (acc, Op::Plus),
+                    "minus" => (acc, Op::Minus),
+                    "divided" => (acc, Op::Divided),
+                    "multiplied" => (acc, Op::Multiplied),
+                    _ => (0, Op::Error),
+                },
+                Op::Start => match val.parse::<i32>() {
+                    Err(_) => (0, Op::Error),
+                    Ok(v) => (v, Op::Next),
+                },
+                Op::Plus => match val.parse::<i32>() {
+                    Err(_) => (0, Op::Error),
+                    Ok(v) => (acc + v, Op::Next),
+                },
+                Op::Minus => match val.parse::<i32>() {
+                    Err(_) => (0, Op::Error),
+                    Ok(v) => (acc - v, Op::Next),
+                },
+                Op::Multiplied => match val.parse::<i32>() {
+                    Err(_) => (0, Op::Error),
+                    Ok(v) => (acc * v, Op::Next),
+                },
+                Op::Divided => match val.parse::<i32>() {
+                    Err(_) => (0, Op::Error),
+                    Ok(v) => (acc / v, Op::Next),
+                },
+            });
 
-
-                     match &result.1 {
-                         &Op::Error => WordProblem { result : Err("Bad command") }, 
-                         _ => WordProblem { result : Ok(result.0) }, 
-                     }
+        // Return a WordProblem depending on if the result returned a (i32, _) tuple of a (_,
+        // Op::Error) tuple
+        match result {
+            (_, Op::Error) => WordProblem {
+                result: Err("Bad command"),
+            },
+            (val, _) => WordProblem { result: Ok(val) },
+        }
     }
 
+    //
+    // Return the result value
+    //
     pub fn answer(&self) -> Result<i32, &'a str> {
         self.result
     }
 
+    //
+    // Return true if evaluation resulted in error
+    //
     pub fn is_err(&self) -> bool {
         match self.result {
             Err(_) => true,
             Ok(_) => false,
         }
     }
-
-
-
 }
-
